568b044e34a7b3e9ac0d960089f9fdb73ebaea83
Mitsuru Utsugi <mitsuru.8914112@gmail.com>
2024-03-05 22:24:50 +0900

    240305-isss001

--- include/Kernel.h
+++ include/Kernel.h
@@ -5,42 +5,59 @@
 class Kernel {
 
 protected:
+	// number of grid
 	size_t		_nx_;
 	size_t		_ny_;
 	size_t		_nz_;
 
+	// range of model space
 	double		*_xx_;
 	double		*_yy_;
 	double		*_zz_;
 
+	// grid object for model space
 	grid		*_grd_;
+	// observed data
 	data_array	*_data_;
 
+	// kernel matrix
 	mm_real		*_K_;
 
+	// mgcal func for computing kernel matrix
 	mgcal_func	*_func_;
 
+	// unit vector parallel to external field and magnetization
 	vector3d	*_exf_;
 	vector3d	*_mgz_;
 
 public:
-	Kernel () { _init_ (); }
+	Kernel () { __init__ (); }
 
+	// set range and creates grid for model space
 	void	set_range (size_t nx, size_t ny, size_t nz, double *xx, double *yy, double *zz);
 	void	set_range (size_t nx, size_t ny, size_t nz, double *xx, double *yy, double *zz, const double ll);
+	// set surface topography
 	void	set_surface (double *zsurf) { grid_set_surface (_grd_, zsurf); }
+	// set observed data
 	void	set_data (data_array *array);
 
+	// return grid object for model space
 	grid	*get_grid () { return _grd_; }
 
-	void	eval ();
+	// compute and return kernel matrix
 	mm_real	*get ();
 
+	// fwrite model
 	void	fwrite (FILE *stream, mm_real *model);
 	void	fwrite (FILE *stream, mm_real *model, const char *format);
 
 protected:
-	void	_init_ ();
+	// evaluate kernel matrix
+	void	_eval_ ();
+
+private:
+	// initializer
+	void	__init__ ();
 };
 
 /*** magnetic kernel ***/
@@ -51,6 +68,7 @@ public:
 	MagKernel (double inc, double dec);
 	MagKernel (double exf_inc, double exf_dec, double mgz_inc, double mgz_dec);
 
+	// set unit vector parallel to external field and magnetization
 	void	set_exf (double inc, double dec);
 	void	set_mgz (double inc, double dec);
 };
--- src/Kernel.cc
+++ src/Kernel.cc
@@ -5,9 +5,74 @@
 #include "Kernel.h"
 #include "gravity.h"
 
-/*** super class for kernel ***/
+/*** public methods ***/
+// set range and creates grid for model space
 void
-Kernel::_init_ ()
+Kernel::set_range (size_t nx, size_t ny, size_t nz, double *xx, double *yy, double *zz)
+{
+	_nx_ = nx;
+	_ny_ = ny;
+	_nz_ = nz;
+	_grd_ = grid_new (_nx_, _ny_, _nz_, xx, yy, zz);
+	grid_stretch_at_edge (_grd_, 1000.);
+}
+
+void
+Kernel::set_range (size_t nx, size_t ny, size_t nz, double *xx, double *yy, double *zz, const double ll)
+{
+	_nx_ = nx;
+	_ny_ = ny;
+	_nz_ = nz;
+	_grd_ = grid_new (_nx_, _ny_, _nz_, xx, yy, zz);
+	if (ll > 0.) grid_stretch_at_edge (_grd_, ll);
+}
+
+// set observed data
+void
+Kernel::set_data (data_array *data)
+{
+	_data_ = data;
+}
+
+// compute and return kernel matrix
+mm_real *
+Kernel::get ()
+{
+	if (_K_ == NULL) _eval_ ();
+	return _K_;
+}
+
+// fwrite model
+void
+Kernel::fwrite (FILE *stream, mm_real *model)
+{
+	fwrite_grid_with_data (stream, _grd_, model->data, "%.4e\t%.4e\t%.4e\t%.8e");
+}
+
+void
+Kernel::fwrite (FILE *stream, mm_real *model, const char *format)
+{
+	fwrite_grid_with_data (stream, _grd_, model->data, format);
+}
+
+/*** protected methods ***/
+// evaluate kernel matrix
+void
+Kernel::_eval_ ()
+{
+	if (_data_ == NULL) throw std::runtime_error ("please set data_array.");
+	if (_grd_ == NULL)   throw std::runtime_error ("please set range.");
+
+	size_t	m = _data_->n;
+	size_t	n = _grd_->n;
+	_K_ = mm_real_new (MM_REAL_DENSE, MM_REAL_GENERAL, m, n, m * n);
+	kernel_matrix_set (_K_->data, _data_, _grd_, _mgz_, _exf_, _func_);
+}
+
+/*** private methods ***/
+// initializer
+void
+Kernel::__init__ ()
 {
 	_nx_ = -1;
 	_ny_ = -1;
@@ -24,67 +89,12 @@ Kernel::_init_ ()
 	_mgz_ = NULL;
 }
 
-void
-Kernel::set_range (size_t nx, size_t ny, size_t nz, double *xx, double *yy, double *zz)
-{
-	_nx_ = nx;
-	_ny_ = ny;
-	_nz_ = nz;
-	_grd_ = grid_new (_nx_, _ny_, _nz_, xx, yy, zz);
-}
-
-void
-Kernel::set_range (size_t nx, size_t ny, size_t nz, double *xx, double *yy, double *zz, const double ll)
-{
-	_nx_ = nx;
-	_ny_ = ny;
-	_nz_ = nz;
-	_grd_ = grid_new (_nx_, _ny_, _nz_, xx, yy, zz);
-	grid_stretch_at_edge (_grd_, ll);
-}
-
-void
-Kernel::set_data (data_array *data)
-{
-	_data_ = data;
-}
-
-void
-Kernel::eval ()
-{
-	if (_data_ == NULL) throw std::runtime_error ("please set data_array.");
-	if (_grd_ == NULL)   throw std::runtime_error ("please set range.");
-
-	size_t	m = _data_->n;
-	size_t	n = _grd_->n;
-	_K_ = mm_real_new (MM_REAL_DENSE, MM_REAL_GENERAL, m, n, m * n);
-	kernel_matrix_set (_K_->data, _data_, _grd_, _mgz_, _exf_, _func_);
-}
-
-mm_real *
-Kernel::get ()
-{
-	if (_K_ == NULL) eval ();
-	return _K_;
-}
-
-void
-Kernel::fwrite (FILE *stream, mm_real *model)
-{
-	fwrite_grid_with_data (stream, _grd_, model->data, "%.4e\t%.4e\t%.4e\t%.8e");
-}
-
-void
-Kernel::fwrite (FILE *stream, mm_real *model, const char *format)
-{
-	fwrite_grid_with_data (stream, _grd_, model->data, format);
-}
-
 /*** magnetic kernel ***/
 MagKernel::MagKernel (double inc, double dec)
 {
 	_exf_ = vector3d_new_with_geodesic_poler (1., inc, dec);
 	_mgz_ = vector3d_new_with_geodesic_poler (1., inc, dec);
+	// set mgcal_func to total_force_prism
 	_func_ = mgcal_func_new (total_force_prism, NULL);
 }
 
@@ -110,6 +120,7 @@ MagKernel::set_mgz (double inc, double dec)
 /*** gravity kernel ***/
 GravKernel::GravKernel ()
 {
+	// set mgcal func to grav_prism
 	_func_ = mgcal_func_new (grav_prism, NULL);
 }
 
