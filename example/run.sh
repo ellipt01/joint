9c6a5d556b18f4034205f2e7776d448c713f8b9a
Mitsuru Utsugi <utsugi@mountain.ocn.ne.jp>
2026-03-05 12:38:10 +0900

    260305-iss001

---
+++ example/run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ ! -e ../bin/jinv ]; then
+	make clean -C ..
+	make -C ..
+	make install -C ..
+	make clean-objs -C ..
+fi
+
+../bin/jinv -f synth/input_mag.in -g synth/input_grv.in -l 0:-1 -s settings.par -v 2>&1 | tee log
+
